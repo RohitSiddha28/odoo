bcc27bb608810bf592dd7756ac1d86adf238d587
Abhidipta-Roy <abhidipta2004@gmail.com>
2026-07-12 10:24:19 +0530

    Add vehicle and report backend APIs

---
+++ backend/controllers/vehicleController.js
@@ -0,0 +1,23 @@
+exports.createVehicle = async (req, res) => {
+    // create vehicle
+};
+
+exports.getVehicles = async (req, res) => {
+    // get all vehicles
+};
+
+exports.getVehicleById = async (req, res) => {
+    // get vehicle
+};
+
+exports.updateVehicle = async (req, res) => {
+    // update vehicle
+};
+
+exports.deleteVehicle = async (req, res) => {
+    // delete vehicle
+};
+
+exports.updateVehicleStatus = async (req, res) => {
+    // update status
+};
